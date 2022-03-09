e513c50ee9617bf2e9eb6bee80ca677f0593a601
dymafr <erwan1309@hotmail.com>
2022-03-09 11:44:27 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  <input type="text" v-model="author.name" />
+  <button @click="count++">{{ count }}</button>
   <div>
     <Blog />
   </div>
 </template>
 
 <script setup lang="ts">
-import { provide, reactive, readonly } from 'vue';
+import { toRef, reactive, isReactive, isRef } from 'vue';
 import Blog from './Blog.vue';
+
+const state = reactive({
+  count: 0,
+  user: {},
+});
+
+const count = toRef(state, 'count');
+
+console.log(isReactive(state));
+console.log(isRef(state));
+console.log(isRef(count));
 </script>
 
 <style scoped lang="scss"></style>
